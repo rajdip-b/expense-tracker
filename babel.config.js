6c1a7e014535cf1c979ec91de979bd2c568c977b
rajdip1 <rajdip@trainingmug.com>
2023-02-19 23:44:21 +0530

    Initial setup

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = function (api) {
+    api.cache(true);
+    return {
+        presets: ['module:babel-preset-expo'],
+        plugins: ['nativewind/babel', 'module:react-native-dotenv'],
+    };
 };
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    assets: ['./assets/fonts/'],
+    content: ['./components/**/*.tsx', './screens/**/*.tsx', './App.tsx'],
+    theme: {
+        extend: {
+            colors: {
+                white: '#ECECEC',
+                lightGray: '#C0B3CE',
+                darkGray: '#57565E',
+                black: '#09080C',
+                brown: '#4B3430',
+                lightGreen: '#ADCCB6',
+                darkGreen: '#7D8A5F',
+            },
+        },
+    },
+    plugins: [],
+};
